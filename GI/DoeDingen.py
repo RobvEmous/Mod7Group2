d04c9333302d59c3e728483975c7da36a6d89c60
Rob van Emous <robvemous@gmail.com>
2015-02-20 00:52:56 +0100

    Speedup: from O(n) or O(n^2) to O(log n)

--- GI/DoeDingen.py
+++ GI/DoeDingen.py
@@ -4,7 +4,7 @@ from graphIO import writeDOT, loadgraph
 
 def doeAlles():
     # laad de grafen
-    L = loadgraph('Graphs/crefBM_2_49.grl',readlist=True)
+    L = loadgraph('Graphs/crefBM_4_4098.grl',readlist=True)
     graph_list = L[0]
     # kleur alle nodes naar aanleiding van hun degree (aantal buren)
     # Optimalisaties:   - alle nodes met degree 1 worden verwijderd
@@ -13,7 +13,9 @@ def doeAlles():
     # Hierna wordt de lijst van nodes gesorteerd op kleur
     maxDeg = []
     sortedVertices = [[]]*len(graph_list)
+    print('- Initial coloring...')
     for i in range(0, len(graph_list)):
+        print(i, ' of ', len(graph_list))
         maxDeg.append(0)
         for j in range(0, len(graph_list[i].V())):
             currDeg = graph_list[i].V()[j].deg()
@@ -25,24 +27,30 @@ def doeAlles():
         writeDOT(graph_list[i],('Results/colorful_' + str(i) + '.dot'))
 
     converged = False
-    hasDupColors = False
+    hasDupColors = []
     iteration_counter = 0
+    print('- Recursive coloring...')
     while not converged:
+        print('Iteration ', iteration_counter)
         for i in range(0, len(graph_list)):
-            converged, hasDupColors, maxDeg[i] = color_Nbss(sortedVertices[i], maxDeg[i])
+            print(i, ' of ', len(graph_list))
+            converged, hasDups, maxDeg[i] = color_Nbss(sortedVertices[i], maxDeg[i])
+            hasDupColors.append(hasDups)
             # update colors of nodes in graph
-            for j in range(0, len(sortedVertices[i])):
-                graph_list[i].V()[j].colornum = merge_sort_label(sortedVertices[i])[graph_list[i].V()[j]._label].colornum
+            sortedToLabelVertices = merge_sort_label(sortedVertices[i])
+            for j in range(0, len(sortedToLabelVertices)):
+                graph_list[i].V()[j].colornum = sortedToLabelVertices[graph_list[i].V()[j]._label].colornum
             sortedVertices[i] = merge_sort_color(graph_list[i].V())
             writeDOT(graph_list[i],('Results/colorfulIt_' + str(i) + '_' + str(iteration_counter) + '.dot'))
         iteration_counter += 1
-    if hasDupColors:
-        print('Dups found: problem for now')
-        return
+    print('- Matching graphs...')
     for i in range(0, len(graph_list)):
-        for j in range(i + 1, len(graph_list)):
-            if maxDeg[i] == maxDeg[j]:
-                print('Iso found:   ', i, ' and ', j)
+        if hasDupColors[i]:
+            print('Dups found in ', i, ' : problem for now')
+        else:
+            for j in range(i + 1, len(graph_list)):
+                if maxDeg[i] == maxDeg[j]:
+                    print('Iso found:   ', i, ' and ', j)
 
 def color_Nbss(sortedVertices, maxDeg):
     converged = True
@@ -50,25 +58,26 @@ def color_Nbss(sortedVertices, maxDeg):
 
     startColor = sortedVertices[0].colornum
     startNbss = merge_sort_color(sortedVertices[0].nbs())
-    colorChangedNodes = []
+    colorChangedNeigbors = []
     for i in range(1, len(sortedVertices)):
+        currNbs = merge_sort_color(sortedVertices[i].nbs())
         if sortedVertices[i].colornum == startColor:
             hasDupColors = True
             changed = False
-            if len(colorChangedNodes) > 0:
-                for j in range(0, len(colorChangedNodes)):
-                    if compareLists(merge_sort_color(colorChangedNodes[j].nbs()), merge_sort_color(sortedVertices[i].nbs())):
-                        sortedVertices[i].colornum = colorChangedNodes[j].colornum
+            if len(colorChangedNeigbors) > 0:
+                for j in range(0, len(colorChangedNeigbors)):
+                    if compareLists(colorChangedNeigbors[j][1], currNbs):
+                        sortedVertices[i].colornum = colorChangedNeigbors[j][0].colornum
                         changed = True
                         converged = False
-            if not changed and not compareLists(startNbss, merge_sort_color(sortedVertices[i].nbs())):
+            if not changed and not compareLists(startNbss, currNbs):
                 maxDeg += 1
                 sortedVertices[i].colornum = maxDeg
-                colorChangedNodes.append(sortedVertices[i])
+                colorChangedNeigbors.append((sortedVertices[i], currNbs))
                 converged = False
         else:
             startColor = sortedVertices[i].colornum
-            startNbss = merge_sort_color(sortedVertices[i].nbs())
+            startNbss = currNbs
             colorChangedNodes = []
     return converged, hasDupColors, maxDeg
 
--- GI/basicgraphs.py
+++ GI/basicgraphs.py
@@ -7,234 +7,335 @@ The interface of these classes is extensive and allows programming all kinds of 
 
 However, the data structure used is quite basic and inefficient: a graph object stores only a vertex list and an edge list, and methods such as adjacency testing / finding neighbors of a vertex require going through the entire edge list!
 """
-# version: 29-01-2015, Paul Bonsma
+# version: 29-01-2015, Paul Bonsma & Rob & Joshua
 
-
-unsafe=False	
+unsafe=False
 # Set to True for faster, but unsafe listing of all vertices and edges.
 
 class GraphError(Exception):
-	def __init__(self,message):
-		self.mess=message
-		
-	def __str__(self):
-		return self.mess
-		
+    def __init__(self,message):
+        self.mess=message
+
+    def __str__(self):
+        return self.mess
+
 
 class vertex():
-	"""
-	Vertex objects have an attribute <_graph> pointing to the graph they are part of, 
-	and an attribute <_label> which can be anything: it is not used for any methods,
-	except for __repr__. 
-	"""
-	def __init__(self,graph,label=0):
-		"""
-		Creates a vertex, part of <graph>, with optional label <label>.
-		(Labels of different vertices may be chosen the same; this does
-		not influence correctness of the methods, but will make the string
-		representation of the graph ambiguous.)
-		"""
-		self._graph=graph
-		self._label=label
-		
-	def __repr__(self):
-		return str(self._label)		
-		
-	def adj(self,other):
-		"""
-		Returns True iff vertex <self> is adjacent to <other> vertex.
-		"""
-		return self._graph.adj(self,other)
+    """
+    Vertex objects have an attribute <_graph> pointing to the graph they are part of,
+    and an attribute <_label> which can be anything: it is not used for any methods,
+    except for __repr__.
+    """
+    def __init__(self,graph,label=0):
+        """
+        Creates a vertex, part of <graph>, with optional label <label>.
+        (Labels of different vertices may be chosen the same; this does
+        not influence correctness of the methods, but will make the string
+        representation of the graph ambiguous.)
+        """
+        self._graph=graph
+        self._label=label
 
-	def inclist(self):
-		"""
-		Returns the list of edges incident with vertex <self>.
-		"""
-		incl=[]
-		for e in self._graph._E:
-			if e.incident(self):
-				incl.append(e)
-		return incl
-		
-	def nbs(self):
-		"""
-		Returns the list of neighbors of vertex <self>.
-		In case of parallel edges: duplicates are not removed from this list!
-		"""		
-		nbl=[]
-		for e in self.inclist():
-			nbl.append(e.otherend(self))
-		return nbl
+    def __repr__(self):
+        return str(self._label)
 
-	def deg(self):
-		"""
-		Returns the degree of vertex <self>.
-		"""
-		return len(self.inclist())
-		
+    def get_label(self):
+        return self._label
+
+    def adj(self,other):
+        """
+        Returns True iff vertex <self> is adjacent to <other> vertex.
+        """
+        return self._graph.adj(self,other)
+
+    def inclist(self):
+        """
+        Returns the list of edges incident with vertex <self>.
+
+        old algorithm:
+
+        incl=[]
+        for e in self._graph._E:
+            if e.incident(self):
+                incl.append(e)
+        """
+        incl=[]
+        index = self._graph.findFirstEdgeOfVertex(self._label)
+        while True:
+            edge = self._graph.E()[index]
+            if edge._tail._label == self._label:
+                incl.append(edge)
+                if index < len(self._graph.E()) - 1:
+                    index += 1
+                else:
+                    return incl
+            else:
+                return incl
+        return incl
+
+    def nbs(self):
+        """
+        Returns the list of neighbors of vertex <self>.
+        In case of parallel edges: duplicates are not removed from this list!
+        """
+        nbl=[]
+        for e in self.inclist():
+            nbl.append(e.otherend(self))
+        return nbl
+
+    def deg(self):
+        """
+        Returns the degree of vertex <self>.
+        """
+        return len(self.inclist())
+
 class edge():
-	"""
-	Edges have attributes <_tail> and <_head> which point to the end vertices 
-	(vertex objects). The order of these is arbitrary (undirected edges).
-	"""
-	def __init__(self,tail,head):
-		"""
-		Creates an edge between vertices <tail> and <head>.
-		"""
-		# tail and head must be vertex objects.
-		if not tail._graph==head._graph:
-			raise GraphError(
-				'Can only add edges between vertices of the same graph')
-		self._tail=tail
-		self._head=head
-		
-	def __repr__(self):
-		return '('+str(self._tail)+','+str(self._head)+')'
-		
-	def tail(self):
-		return self._tail
-		
-	def head(self):
-		return self._head
-		
-	def otherend(self,oneend):
-		"""
-		Given one end vertex <oneend> of the edge <self>, this returns
-		the other end vertex of <self>.
-		"""
-		# <oneend> must be either the head or the tail of this edge.
-		if self._tail==oneend:
-			return self._head
-		elif self._head==oneend:
-			return self._tail
-		raise GraphError(
-			'edge.otherend(oneend): oneend must be head or tail of edge')
-		
-	def incident(self,vertex):
-		"""
-		Returns True iff the edge <self> is incident with the 
-		vertex <vertex>.
-		"""
-		if self._tail==vertex or self._head==vertex:
-			return True
-		else:
-			return False
-		
+    """
+    Edges have attributes <_tail> and <_head> which point to the end vertices
+    (vertex objects). The order of these is arbitrary (undirected edges).
+    """
+    def __init__(self,tail,head):
+        """
+        Creates an edge between vertices <tail> and <head>.
+        """
+        # tail and head must be vertex objects.
+        if not tail._graph==head._graph:
+            raise GraphError(
+                'Can only add edges between vertices of the same graph')
+        self._tail=tail
+        self._head=head
+
+    def __repr__(self):
+        return '('+str(self._tail)+','+str(self._head)+')'
+
+    def tail(self):
+        return self._tail
+
+    def head(self):
+        return self._head
+
+    def otherend(self,oneend):
+        """
+        Given one end vertex <oneend> of the edge <self>, this returns
+        the other end vertex of <self>.
+        """
+        # <oneend> must be either the head or the tail of this edge.
+        if self._tail==oneend:
+            return self._head
+        elif self._head==oneend:
+            return self._tail
+        raise GraphError(
+            'edge.otherend(oneend): oneend must be head or tail of edge')
+
+    def incident(self,vertex):
+        """
+        Returns True iff the edge <self> is incident with the
+        vertex <vertex>.
+        """
+        if self._tail==vertex or self._head==vertex:
+            return True
+        else:
+            return False
+
 class graph():
-	"""
-	A graph object has as main attributes:
-	 <_V>: the list of its vertices
-	 <_E>: the list of its edges
-	In addition:
-	 <_simple> is True iff the graph must stay simple (used when trying to add edges)
-	 <_directed> is False for now (feel free to write a directed variant of this
-	 	module)
-	 <_nextlabel> is used to assign default labels to vertices.
-	"""
-	def __init__(self,n=0,simple=False):
-		"""
-		Creates a graph. 
-		Optional argument <n>: number of vertices.
-		Optional argument <simple>: indicates whether the graph should stay simple.
-		"""
-		self._V=[]
-		self._E=[]
-		self._directed=False
-		# may be changed later for a more general version that can also 
-		# handle directed graphs.
-		self._simple=simple
-		self._nextlabel=0
-		for i in range(n):
-			self.addvertex()
-		
-	def __repr__(self):
-		return 'V='+str(self._V)+'\nE='+str(self._E)	
+    """
+    A graph object has as main attributes:
+     <_V>: the list of its vertices
+     <_E>: the list of its edges, these are sorted when used for searching (for O(log n) instead of O(n) performance)
+    In addition:
+     <_simple> is True iff the graph must stay simple (used when trying to add edges)
+     <_directed> is False for now (feel free to write a directed variant of this
+         module)
+     <_nextlabel> is used to assign default labels to vertices.
+    """
+    unsorted = False
 
-	def V(self):
-		"""
-		Returns the list of vertices of the graph.
-		"""
-		if unsafe:	# but fast
-			return self._V
-		else:
-			return self._V[:]	# return a *copy* of this list
-			
-	def E(self):
-		"""
-		Returns the list of edges of the graph.
-		"""
-		if unsafe:	# but fast
-			return self._E
-		else:
-			return self._E[:]	# return a *copy* of this list
-	
-	def __getitem__(self,i):
-		"""
-		Returns the <i>th vertex of the graph -- as given in the vertex list; 
-		this is not related to the vertex labels.
-		"""
-		return self._V[i]
-	
-	def addvertex(self,label=-1):
-		"""
-		Add a vertex to the graph. 
-		Optional argument: a vertex label (arbitrary)
-		"""
-		if label==-1:
-			label=self._nextlabel
-			self._nextlabel+=1
-		u=vertex(self,label)
-		self._V.append(u)
-		return u
-		
-	def addedge(self,tail,head):
-		"""
-		Add an edge to the graph between <tail> and <head>.	
-		Includes some checks in case the graph should stay simple.
-		"""
-		if self._simple:
-			if tail==head:
-				raise GraphError('No loops allowed in simple graphs')
-			for e in self._E:
-				if (e._tail==tail and e._head==head):
-					raise GraphError(
-						'No multiedges allowed in simple graphs')
-				if not self._directed:
-					if (e._tail==head and e._head==tail):
-						raise GraphError(
-							'No multiedges allowed in simple graphs')
-		if not (tail._graph==self and head._graph==self):
-			raise GraphError(
-				'Edges of a graph G must be between vertices of G')
-		e=edge(tail,head)
-		self._E.append(e)
-		return e
+    def __init__(self,n=0,simple=False):
+        """
+        Creates a graph.
+        Optional argument <n>: number of vertices.
+        Optional argument <simple>: indicates whether the graph should stay simple.
+        """
+        self._V=[]
+        self._E=[]
+        self._directed=False
+        # may be changed later for a more general version that can also
+        # handle directed graphs.
+        self._simple=simple
+        self._nextlabel=0
+        for i in range(n):
+            self.addvertex()
 
-	def findedge(self,u,v):
-		"""
-		If <u> and <v> are adjacent, this returns an edge between them.
-		(Arbitrary in the case of multigraphs.)
-		Otherwise this returns <None>.
-		"""
-		for e in self._E:
-			if (e._tail==u and e._head==v) or (e._tail==v and e._head==u):
-				return e
-		return None
-		
+    def __repr__(self):
+        return 'V='+str(self._V)+'\nE='+str(self._E)
 
-	def adj(self,u,v):
-		"""
-		Returns True iff vertices <u> and <v> are adjacent.
-		"""
-		if self.findedge(u,v)==None:
-			return False
-		else:
-			return True
-		
-	def isdirected(self):
-		"""
-		Returns False, because for now these graphs are always undirected.
-		"""
-		return self._directed
+    def V(self):
+        """
+        Returns the list of vertices of the graph.
+        """
+        if unsafe:	# but fast
+            return self._V
+        else:
+            return self._V[:]	# return a *copy* of this list
+
+    def E(self):
+        """
+        Returns the list of edges of the graph.
+        """
+        if unsafe:	# but fast
+            return self._E
+        else:
+            return self._E[:]	# return a *copy* of this list
+
+    def __getitem__(self,i):
+        """
+        Returns the <i>th vertex of the graph -- as given in the vertex list;
+        this is not related to the vertex labels.
+        """
+        return self._V[i]
+
+    def addvertex(self,label=-1):
+        """
+        Add a vertex to the graph.
+        Optional argument: a vertex label (arbitrary)
+        """
+        if label==-1:
+            label=self._nextlabel
+            self._nextlabel+=1
+        u=vertex(self,label)
+        self._V.append(u)
+        return u
+
+    def addedge(self,tail,head):
+        """
+        Add an edge to the graph between <tail> and <head>.
+        Includes some checks in case the graph should stay simple.
+        """
+        if tail.get_label() > head.get_label(): # nessesary for sorting but makes graph undirected by default
+            tail, head = head, tail
+        if self._simple:
+            if tail==head:
+                raise GraphError('No loops allowed in simple graphs')
+            for e in self._E:
+                if (e._tail==tail and e._head==head):
+                    raise GraphError(
+                        'No multiedges allowed in simple graphs')
+                if not self._directed:
+                    if (e._tail==head and e._head==tail):
+                        raise GraphError(
+                            'No multiedges allowed in simple graphs')
+        if not (tail._graph==self and head._graph==self):
+            raise GraphError(
+                'Edges of a graph G must be between vertices of G')
+        e=edge(tail,head)
+        self._E.append(e)
+        self.unsorted = True
+        return e
+
+    def sortEdgesRec(self, list):
+        if len(list) > 1:
+            i = int((len(list) / 2))
+            f = self.sortEdgesRec(list[:i])
+            s = self.sortEdgesRec(list[i:])
+            r = []
+            fi = si = 0
+            while fi < len(f) and si < len(s):
+                if f[fi].head().get_label() < s[si].head().get_label() or f[fi].head().get_label() == s[si].head().get_label() and f[fi].tail().get_label() < s[si].tail().get_label():
+                    r.append(f[fi])
+                    fi += 1
+                else:
+                    r.append(s[si])
+                    si += 1
+            if fi < len(f):
+                r += f[fi:]
+            elif si < len(s):
+                r += s[si:]
+            return r
+        else:
+            return list
+
+    def sortEdges(self):
+        self.sortEdgesRec(self._E)
+        self.unsorted = False
+
+    # uses binary algorithm to search for (first) elements in lists containing tupels
+    def binary_search_pairs(self, list, value):
+        if len(list) > 0:
+            l = 0
+            h = len(list) - 1
+            while h - l > 0 and list[l].tail().get_label() != value:
+                m = int((l + h) / 2)
+                if list[m].tail().get_label() == value:
+                    l = m
+                elif list[m].tail().get_label() < value:
+                    l = m + 1
+                else:
+                    h = m - 1
+            if list[l].tail().get_label() == value:
+                return l
+            else:
+                return -1
+        else:
+            return -1
+
+    def findFirstEdgeOfVertex(self, label):
+        if self.unsorted == True:
+            self.sortEdges()
+        return self.binary_search_pairs(self._E, label)
+
+
+    def findedgeRec(self, u, v, max, min):
+        """
+        If <u> and <v> are adjacent, this returns an edge between them.
+        (Arbitrary in the case of multigraphs.)
+        Otherwise this returns <None>.
+
+        for e in self._E:
+            if (e._tail==u and e._head==v) or (e._tail==v and e._head==u):
+                return e
+        return None
+        """
+        print(max)
+        print(min)
+        if(max == min):
+            return None
+        if (u < v):
+            high = v
+            low = u
+        else:
+            high = u
+            low = v
+        e = self._E
+        b = max - min
+        i = (b // 2) + min
+        if e[i][1]==high and e[i][0]==low:
+            return e[i]
+        elif i == len(e) - 1:
+            return None
+        elif e[i][0] < low or e[i][1] < high:
+            return self.findedgeRec(u, v, max, i)
+        elif e[i][0] > low or e[i][1] > high:
+            return self.findedgeRec(u, v, i, min)
+        return None
+
+    def findedge(self,u,v):
+        if self.unsorted == True:
+            self.sortEdges()
+        return self.findedgeRec(u, v, len(self._E), 0)
+
+    def adj(self,u,v):
+        """
+        Returns True iff vertices <u> and <v> are adjacent.
+        """
+        if self.findedge(u,v) == None:
+            return False
+        else:
+            return True
+
+    def isdirected(self):
+        """
+        Returns False, because for now these graphs are always undirected.
+        """
+        return self._directed
 		
 
